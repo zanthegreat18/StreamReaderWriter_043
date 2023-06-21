dc8b6b03e3c033adbb71f320404e10328ee4965c
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-06-21 14:49:48 +0700

    Add project files.

---
+++ StreamOjan/StreamOjan.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	// Membuka file dalam mode menulis.
+	ofstream outfile;
+	// Menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		// menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+			
